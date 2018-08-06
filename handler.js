d7d6e012e3ffc977b65102dfecccda9ca62f0060
Lama <lama.n.alharbi@gmail.com>
2018-08-06 04:37:50 +0300

    First commit

---
+++ handler.js
@@ -0,0 +1,32 @@
+'use strict';
+
+module.exports.hello = (event, context, callback) => {
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: 'Go Serverless v1.0! Your function executed successfully!',
+      input: event,
+    }),
+  };
+
+  callback(null, response);
+
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
+};
+
+
+module.exports.hello = function(event, context, callback) {
+
+    console.log(event); // Contains incoming request data (e.g., query params, headers and more)
+
+    const response = {
+      statusCode: 200,
+      headers: {
+        "x-custom-header" : "My Header Value"
+      },
+      body: JSON.stringify({ "message": "Hello World!" })
+    };
+
+    callback(null, response);
+};
